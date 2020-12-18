25b9e0bf109386576ff66eb08ef57f2a6e34fae9
Abdullahi Usman <abdullahismn7@gmail.com>
2020-12-18 13:18:18 +0100

    fix: show error on production

--- KhemicalKoder/Startup.cs
+++ KhemicalKoder/Startup.cs
@@ -49,9 +49,9 @@ namespace KhemicalKoder
             }
             else
             {
-                //app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
